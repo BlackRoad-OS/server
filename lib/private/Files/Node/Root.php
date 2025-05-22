bfdac862c06a869a327e1838b0d542b9330eb570
Ferdinand Thiessen <opensource@fthiessen.de>
2025-05-22 14:32:50 +0200

    feat(IUserFolder): add a user folder class
    * Implements https://github.com/nextcloud/server/issues/52896
    
    Similar to the root folder this represents the users home folder,
    it allows to group user based methods on the folder like the user quota.
    
    Signed-off-by: Ferdinand Thiessen <opensource@fthiessen.de>

--- lib/private/Files/Node/Root.php
+++ lib/private/Files/Node/Root.php
@@ -22,6 +22,7 @@ use OCP\Files\Cache\ICacheEntry;
 use OCP\Files\Config\IUserMountCache;
 use OCP\Files\Events\Node\FilesystemTornDownEvent;
 use OCP\Files\IRootFolder;
+use OCP\Files\IUserFolder;
 use OCP\Files\Mount\IMountPoint;
 use OCP\Files\Node as INode;
 use OCP\Files\NotFoundException;
@@ -29,6 +30,7 @@ use OCP\Files\NotPermittedException;
 use OCP\IAppConfig;
 use OCP\ICache;
 use OCP\ICacheFactory;
+use OCP\IConfig;
 use OCP\IUser;
 use OCP\IUserManager;
 use OCP\Server;
@@ -67,7 +69,7 @@ class Root extends Folder implements IRootFolder {
 		private LoggerInterface $logger,
 		private IUserManager $userManager,
 		IEventDispatcher $eventDispatcher,
-		ICacheFactory $cacheFactory,
+		private ICacheFactory $cacheFactory,
 		IAppConfig $appConfig,
 	) {
 		parent::__construct($this, $view, '');
@@ -318,12 +320,10 @@ class Root extends Folder implements IRootFolder {
 	/**
 	 * Returns a view to user's files folder
 	 *
-	 * @param string $userId user ID
-	 * @return \OCP\Files\Folder
 	 * @throws NoUserException
 	 * @throws NotPermittedException
 	 */
-	public function getUserFolder($userId) {
+	public function getUserFolder(string $userId): IUserFolder {
 		$userObject = $this->userManager->get($userId);
 
 		if (is_null($userObject)) {
@@ -352,9 +352,23 @@ class Root extends Folder implements IRootFolder {
 					}
 				} catch (NotFoundException $e) {
 					if (!$this->nodeExists('/' . $userId)) {
-						$this->newFolder('/' . $userId);
+						$parent = $this->newFolder('/' . $userId);
+					} else {
+						$parent = $this->get('/' . $userId);
+						if (!$parent instanceof \OCP\Files\Folder) {
+							throw new \Exception("Account folder for \"$userId\" exists as a file");
+						}
 					}
-					$folder = $this->newFolder('/' . $userId . '/files');
+					$realFolder = $this->newFolder('/' . $userId . '/files');
+					$folder = new UserFolder(
+						$this->root,
+						$this->view,
+						$realFolder->getPath(),
+						$parent,
+						Server::get(IConfig::class),
+						$userObject,
+						$this->cacheFactory,
+					);
 				}
 			} else {
 				$folder = new LazyUserFolder($this, $userObject, $this->mountManager, $this->useDefaultHomeFoldersPermissions);
